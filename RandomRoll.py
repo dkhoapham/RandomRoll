3007acc180a5797d90aed917893f836dd745dfb7
Kevin_P <58644950+Khoa-Pham@users.noreply.github.com>
2021-01-22 08:13:06 -0800

    Update RandomRoll.py

--- RandomRoll.py
+++ RandomRoll.py
@@ -7,8 +7,18 @@ max = int(input("Max: "))
 roll_again = "yes"
 
 while roll_again == "yes" or roll_again == "y":
-    print("Rolling the dices......")
-    time.sleep(0.65)
+    print("Rolling the dices", end=''),
+    time.sleep(0.1)
+    print('.', end='')
+    time.sleep(0.15)
+    print('.', end='')
+    time.sleep(0.2)
+    print('.', end='')
+    time.sleep(0.25)
+    print('.', end='')
+    time.sleep(0.3)
+    print('.')
+    time.sleep(0.35)
     print("The wining number is: ", end=''),
     print(random.randint(min, max))
 
